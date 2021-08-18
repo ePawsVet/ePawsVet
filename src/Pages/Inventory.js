6c65cbbe98ba54651983df2cdb1b044de0bdca69
ePawsVet <contact.epaws@gmail.com>
2021-08-18 20:57:14 +0800

    Fixed radio buttons

--- src/Pages/Inventory.js
+++ src/Pages/Inventory.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Navbars from "../Components/Navbars";
+import { Image} from 'react-bootstrap'
 
 export default function Inventory() {
   return (
     <>
         <Navbars title="Inventory"></Navbars>
-        Inventory Here
+        <Image className="webpage-holder" src="https://www.dewebworks.com/images/default-source/page-graphics/under-construction/websiteunavailable.jpg?sfvrsn=af3f4331_0"/>
     </>
   )
 }
--- src/Pages/Medicines.js
+++ src/Pages/Medicines.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Navbars from "../Components/Navbars";
+import { Image} from 'react-bootstrap'
 
 export default function Medicines() {
   return (
     <>
         <Navbars title="Medicines"></Navbars>
-        Medicines Here
+        <Image className="webpage-holder" src="https://www.dewebworks.com/images/default-source/page-graphics/under-construction/websiteunavailable.jpg?sfvrsn=af3f4331_0"/>
     </>
   )
 }
