add593b848de0ab783a68cdb9b4ea7d661c71686
ePawsVet <contact.epaws@gmail.com>
2021-08-20 01:58:05 +0800

    SO MANY THINGS

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Carousel } from 'react-bootstrap'
-import { Button} from 'react-bootstrap'
+import { Navbar,Nav,Container, Button} from 'react-bootstrap'
 
 import Footer from '../Components/Footer';
 import Navbars from '../Components/Navbars';
@@ -9,6 +9,23 @@ export default function Home() {
   return (
     <>
     <Navbars title="Home"></Navbars>
+   {/* <Navbar bg="light" expand="lg">
+  <Container>
+    <Navbar.Brand href="#home">ePaws</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+      </Nav>
+      <Nav >
+      <Nav.Link href="/login">Login</Nav.Link>
+        <Nav.Link href="/signup">Register</Nav.Link>
+        <Nav.Link href="/about">About</Nav.Link>
+        <Nav.Link href="/contact">Contact Us</Nav.Link>
+        
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar> */}
   <section className="banner-area" id="home">
   </section>
   <div id="cover-photo">
