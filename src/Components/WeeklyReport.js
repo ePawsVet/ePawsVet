1da9c18aeb445b5b71ac105a3456a058e251636f
ePawsVet <contact.epaws@gmail.com>
2021-12-01 21:14:04 +0800

    added export to csv

--- src/Components/WeeklyReport.js
+++ src/Components/WeeklyReport.js
@@ -5,12 +5,15 @@ import "antd/dist/antd.css";
 import { db } from '../firebase';
 import moment from 'moment';
 import $ from 'jquery'
-import { Form,Col } from 'react-bootstrap'
+import { Form, Col, Button, InputGroup } from 'react-bootstrap'
+import { CSVLink } from "react-csv";
+import { FaFileExport } from 'react-icons/fa';
 
 
 export default function WeeklyReport() {
   const [meds, setMeds] = useState(null)
   const [filteredData, setFilteredData] = useState(null)
+  const [csvData, setCsvData] = useState([])
   const columns = [
     {
       title: 'Pet Owner',
@@ -81,6 +84,7 @@ export default function WeeklyReport() {
             }
           })
           setFilteredData(scheds)
+          setCsvData(scheds)
         })
     return subscribe
   }, [])
@@ -89,7 +93,7 @@ export default function WeeklyReport() {
     var frequency = $(".frequency").val()
     var scheds = []
 
-    if (frequency === "daily") {
+    if (frequency === "Daily") {
       let dates = moment().format('L')
       meds.forEach(sched => {
         if (dates === sched.Date) {
@@ -104,7 +108,7 @@ export default function WeeklyReport() {
       })
       setFilteredData(scheds)
     }
-    if (frequency === "weekly") {
+    else if (frequency === "Weekly") {
       let dates = getCurrentWeekDays()
       meds.forEach(sched => {
         if (dates.includes(sched.Date)) {
@@ -119,7 +123,7 @@ export default function WeeklyReport() {
       })
       setFilteredData(scheds)
     }
-    if (frequency === "monthly") {
+    else if (frequency === "Monthly") {
       let dates = getCurrentMonthDays()
       meds.forEach(sched => {
         if (dates.includes(sched.Date)) {
@@ -134,16 +138,21 @@ export default function WeeklyReport() {
       })
       setFilteredData(scheds)
     }
+    setCsvData(scheds)
   }
+  console.log(csvData)
   return (
     <>
       <Form.Group as={Col} controlId="formGridFrequency" className="formGridFrequency">
         <Form.Label>Filter by</Form.Label>
-        <select onChange={() => refreshTable()} className="frequency form-select">
-          <option value="daily">Daily</option>
-          <option value="weekly">Weekly</option>
-          <option value="monthly">Monthly</option>
-        </select>
+        <InputGroup className="mb-3">
+          <select onChange={() => refreshTable()} className="frequency form-select">
+            <option value="Daily">Daily</option>
+            <option value="Weekly">Weekly</option>
+            <option value="Monthly">Monthly</option>
+          </select>
+          <Button disabled={csvData.length > 0 ? false : true}><CSVLink filename={$(".frequency").val() + "_Report"} data={csvData}><FaFileExport /> Export File</CSVLink></Button>
+        </InputGroup>
       </Form.Group>
       <Table columns={columns} dataSource={filteredData} />
     </>
